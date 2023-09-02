a19f30ffa7a329f4bf5a4d7d7e66f099a050b50b
thiagofraanco762 <contato.franco21@gmail.com>
2023-09-02 14:41:28 -0300

    .

---
+++ Cloacker/admin/version.php
@@ -0,0 +1,8 @@
+<h2>
+    <a href="https://yellowweb.top/donate" target="_blank">
+        Version: 01.09.2023
+        <br />
+        PHP: <?= phpversion() ?>
+        <br /> Please, donate!
+    </a>
+</h2>
